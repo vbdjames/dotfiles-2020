c7814641ef045cd229c40eafb960a5d33237fb13
Doug James <djames@manos.mapcom.local>
2016-07-06 12:51:34 -0400

    only copy solarized palette if missing

--- solarized/install.sh
+++ solarized/install.sh
@@ -1,1 +1,4 @@
-wget https://github.com/altercation/solarized/blob/master/apple-colorpalette-solarized/solarized.clr?raw=true -O ~/Library/Colors/Solarized.clr
+if [ ! -f ~/Library/Colors/Solarized.clr ]
+then
+    wget https://github.com/altercation/solarized/blob/master/apple-colorpalette-solarized/solarized.clr?raw=true -O ~/Library/Colors/Solarized.clr
+fi
